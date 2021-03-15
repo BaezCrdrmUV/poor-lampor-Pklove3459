158e0b70cc09684ff911c11b591b57b0e7b40b46
Pklove3459 <61339804+Pklove3459@users.noreply.github.com>
2021-03-14 21:53:24 -0600

    PoorLamport

---
+++ PoorLamport/PoorLamport/Mensaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoorLamport
+{
+    public class Mensaje
+    {
+        public Process Remitente { get; set;}
+        public Process Emisor { get; set; }
+
+        public int Tiempo { get; set; }
+
+        public String Señal { get; set; }
+
+        public Mensaje()
+        {
+            Tiempo = 0;
+        }
+    }
+}
